e552395af0e4b4f5cd7a59b51a4c2fc95af8fef2
sriv95 <srivichai95@gmail.com>
2024-12-24 15:21:29 +0700

    lab12_4.cpp complete

--- lab12_4.cpp
+++ lab12_4.cpp
@@ -2,6 +2,9 @@
 
 using namespace std;
 
+template <typename T>
+void mySwap(T &,T &);
+
 int main(){
 	int x, y;
 	string a, b;
@@ -30,3 +33,9 @@ int main(){
 	return 0;
 }
 
+template <typename Y>
+void mySwap(Y &x,Y &y){
+    Y z = x;
+    x = y;
+    y = z;
+}
