b1622931703e0fdc4c08f856be9a95f037905547
pwdryx <srivichai95@gmail.com>
2024-12-24 16:07:36 +0700

    lab12_1.cpp complete

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -14,7 +14,6 @@ int main(){
 }
 
 long long int fibonacci(int x){
-    if (x==0) return 0;
-    else if (x==1) return 1;
-    else return fibonacci(x-1)+fibonacci(x-2);
+    if (x>1) return fibonacci(x-1)+fibonacci(x-2);
+    else return x;
 }
